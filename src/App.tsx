98c065f89edac00781d194c76e82c31a54b790a3
kali <lordjson8@gmail.com>
2026-04-07 02:34:49 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center bg-gray-600">
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
